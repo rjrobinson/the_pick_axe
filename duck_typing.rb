ac41d5866e5e6e4831559ac762d94528770138c4
RJ Robinson <robertj.robinson@gmail.com>
2014-11-03 21:20:17 -0500

    work on ch 23

---
+++ duck_typing.rb
@@ -0,0 +1,12 @@
+class Customer
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  def append_name_to_file(file)
+    file << @first_name << ' ' << @llast_name
+  end
+
+
+end
